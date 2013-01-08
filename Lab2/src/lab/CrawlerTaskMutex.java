744b2c069ae162384bc053d30ae44b67a80a19dd
Ron Reiter <ron.reiter@gmail.com>
2013-01-08 20:24:13 +0200

    mutex

---
+++ Lab2/src/lab/CrawlerTaskMutex.java
@@ -0,0 +1,28 @@
+package lab;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tamir
+ * Date: 1/6/13
+ * Time: 12:26 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CrawlerTaskMutex {
+    private int counter = 0;
+
+    public int register()
+    {
+        Logger.debug("Crawler Task started got: " + (counter + 1) + " crawls running");
+        return ++counter;
+    }
+    public int unregister()
+    {
+        Logger.debug("Crawler Task finished, got: " + (counter - 1) + " crawls running");
+        return --counter;
+    }
+    public int count()
+    {
+        return counter;
+    }
+
+}
