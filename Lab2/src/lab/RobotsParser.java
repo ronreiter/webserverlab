931f78c95cec2cae08b1e0c8a65d4e9420ad3132
Tamir Berliner <door2k@gmail.com>
2013-01-08 19:29:20 +0200

    Merge branch 'master' of https://github.com/ronreiter/webserverlab
    Conflicts:
    	Lab2/2013-01-06.txt

--- Lab2/src/lab/RobotsParser.java
+++ Lab2/src/lab/RobotsParser.java
@@ -14,23 +14,36 @@ import java.util.List;
  */
 public class RobotsParser {
     List<String> disallowedPaths;
+    List<String> allowedPaths;
 
     public RobotsParser(String robotsFile) {
         disallowedPaths = new LinkedList<String>();
+        allowedPaths = new LinkedList<String>();
 
         for (String line : robotsFile.split("\n")) {
             if (line.startsWith("Disallow:")) {
                 disallowedPaths.add(line.split(":")[1].trim());
+            } else if (line.startsWith("Allow:")) {
+                allowedPaths.add(line.split(":")[1].trim());
             }
         }
     }
 
     public boolean checkURLAllowed(URL url) {
-        for (String path : disallowedPaths) {
+        // First check if the URL has an "Allow:" entry - if so then permit
+        for (String path: allowedPaths) {
             if (url.getPath().startsWith(path)) {
-                return false;
+                return true;
             }
         }
+
+        // If it's not in the Allowed then check if it's Disallowed. if so block.
+        for (String path : disallowedPaths) {
+            if (url.getPath().startsWith(path)) {
+                    return false;
+            }
+        }
+
         return true;
     }
 
@@ -40,10 +53,12 @@ public class RobotsParser {
             "User-agent: *\n" +
             "Disallow: /cgi-bin/\n" +
             "Disallow: /tmp/\n" +
-            "Disallow: /junk/");
+            "Disallow: /junk/\n" +
+            "Allow: /cgi-bin/index.html");
 
         assert parser.checkURLAllowed(new URL("http://www.google.com/allowed"));
         assert !parser.checkURLAllowed(new URL("http://www.google.com/cgi-bin/disallowed"));
+        assert parser.checkURLAllowed(new URL("http://www.google.com/cgi-bin/index.html"));
 
     }
 }
