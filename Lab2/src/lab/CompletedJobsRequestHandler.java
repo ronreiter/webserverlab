8a5e53ba3845f92b9c1117f3c3830ade8cf77d94
Ron Reiter <ron.reiter@gmail.com>
2013-01-09 15:53:10 +0200

    create reports directory

--- Lab2/src/lab/CompletedJobsRequestHandler.java
+++ Lab2/src/lab/CompletedJobsRequestHandler.java
@@ -11,7 +11,13 @@ public class CompletedJobsRequestHandler extends RequestHandler {
 	public void get() {
         Map<String, Object> templateValues = new HashMap<String, Object>();
         StringBuilder fileList = new StringBuilder();
+
         File folder = new File(new File("root"), "reports");
+
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+
         for (File file : folder.listFiles()) {
             fileList.append("<tr><td><a href='/reports/" + file.getName() + "'>" + file.getName() + "</a></td></tr>");
         }
